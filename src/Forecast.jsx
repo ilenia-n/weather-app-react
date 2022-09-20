d6dec2b00e46e48bcbea3a4244ed04286eb22b33
Ilenia <83571246+ilenia-n@users.noreply.github.com>
2022-09-20 11:59:08 +0100

    Added ForecastDays Component
    - added ForecastDays Component and imported to the Forecast one
    - ForecastDays displays the daily forecast cards for the next 5 days only for the default city for now

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,39 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css"
+import ForecastDays  from "./ForecastDays";
 import axios from "axios";
 
 
+
 function Forecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [data, setData] =useState(null);
 
   function performResults(response) {
     console.log(response.data)
+
+    setLoaded(true);
+    setData(response.data.daily);
   }
 
   let lat = props.coord.lat;
   let lon = props.coord.lon;
   let apiURl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=dc51080b02acff683dbe8e4cbf69dccc&units=metric`;
 
-  axios(apiURl).then(performResults);
 
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col">
-          <div className="card">
-            <div className="days-forecast">Sat</div>
-            <img className="week-image"
-              src={require(`./assets/03d.png`
-              )}
-              alt=""
-            />
-            <span className="days-forecast"> 22° </span>
-            <div className="min"> 16°
-            </div>
-          </div>
+  if (loaded) {
+    return ( 
+      <div className="Forecast">
+        <div className="row">
+            <ForecastDays data={data[0]} icon={data[0].weather[0].icon} />
+            <ForecastDays data={data[1]} icon={data[1].weather[0].icon} />
+            <ForecastDays data={data[2]} icon={data[2].weather[0].icon} />
+            <ForecastDays data={data[3]} icon={data[3].weather[0].icon} />
+            <ForecastDays data={data[4]} icon={data[4].weather[0].icon} />
         </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    axios(apiURl).then(performResults);
+  }
+
+
 }
 
 export default Forecast;
---
+++ src/ForecastDays.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+function ForecastDays (props){
+
+    function returnDay() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+        let weekdays= ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        return weekdays[day];
+    }
+
+    function maxTemp(){
+        let maxTemp = Math.round(props.data.temp.max);
+        return maxTemp;
+    }
+
+    function minTemp(){
+        let minTemp = Math.round(props.data.temp.min);
+        return minTemp;
+    }
+
+ return (
+    <div className="col">
+    <div className="card">
+    <div className="days-forecast">{returnDay()}</div>
+    <img className="week-image"
+      src={require(`./assets/${props.icon}.png`)}
+      alt=""
+    />
+    <span className="days-forecast">{maxTemp()}°</span>
+    <div className="min">{minTemp()}°
+    </div>
+    </div>
+  </div>
+ )
+}
+
+export default ForecastDays;
