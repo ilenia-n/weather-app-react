d38f47c85c453811f33cd2d292fd237738b0957f
Ilenia <83571246+ilenia-n@users.noreply.github.com>
2022-10-25 14:47:22 +0100

    Updated footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Weather defaultcity="london" />
-      <footer> <a href="https://github.com/ilenia-n/weather-app-react" target="_blank" rel="noopener noreferrer" >Open-source code </a>by Ilenia Natoli</footer>
+      <footer> <a href="https://github.com/ilenia-n/weather-app-react" target="_blank" rel="noopener noreferrer" >Open-source code </a>by Ilenia</footer>
     </div>
   );
 }
